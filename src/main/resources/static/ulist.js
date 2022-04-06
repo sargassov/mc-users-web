6d0214e667f4143d9460793376386747fef7d434
sargassov <sargassov@yandex.ru>
2022-04-06 17:44:47 +0300

    Работа с фронтом

--- src/main/resources/static/ulist.js
+++ src/main/resources/static/ulist.js
@@ -1,13 +1,20 @@
-angular.module('index', ['ngStorage']).controller('indexController', function ($scope, $rootScope, $http, $localStorage) {
+angular.module('ulist', ['ngStorage']).controller('ulistController', function ($scope, $rootScope, $http, $localStorage) {
     const contextPath = 'http://localhost:8189/app';
 
-    if ($localStorage.springWebUser) {
-        $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.springWebUser.token;
-    }
+     if ($localStorage.springWebUser) {
+        $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.springWebUser.token
+     }
 
+
+    $scope.loadUsers = function () {
+        $http.get(contextPath + '/users')
+            .then(function (response) {
+                $scope.UserList = response.data;
+            });
+    };
 
     $scope.tryToAuth = function () {
-        $http.post('http://localhost:8189/app/auth', $scope.user)
+        $http.post(contextPath + '/auth', $scope.user)
             .then(function successCallback(response) {
                 if (response.data.token) {
                     $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
@@ -45,11 +52,13 @@ angular.module('index', ['ngStorage']).controller('indexController', function ($
     };
 
     $scope.showCurrentUserInfo = function () {
-        $http.get('http://localhost:8189/app/api/v1/profile')
+        $http.get(contextPath + '/profile')
             .then(function successCallback(response) {
                 alert('MY NAME IS: ' + response.data.username);
             }, function errorCallback(response) {
                 alert('UNAUTHORIZED');
             });
     }
+
+    $scope.loadUsers();
 });
